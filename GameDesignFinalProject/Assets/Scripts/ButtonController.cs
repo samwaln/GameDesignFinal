12dc6eeb47e57a51de6471430bb143ecd8ef0ece
Samantha Waln <samywaln@gmail.com>
2020-04-02 00:12:17 -0700

    new branch without big files

---
+++ GameDesignFinalProject/Assets/Scripts/ButtonController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ButtonController : MonoBehaviour
+{
+    public Rigidbody2D princess;
+    bool moving;
+    Vector3 move;
+
+    private void Start()
+    {
+        moving = false;
+        //princess = GetComponent<Rigidbody2D>();
+    }
+
+    public void RunGame()
+    {
+        moving = true;
+    }
+
+    public void Update()
+    {
+        move = Vector3.zero;
+        if (moving)
+        {
+            move += transform.right;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (moving)
+        {
+            princess.transform.position += this.transform.right * Time.deltaTime * 0.7f;
+        }
+        //princess.AddForce(move * 3);
+    }
+}
---
+++ GameDesignFinalProject/Assets/Scripts/LadderController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LadderController : MonoBehaviour
+{
+    Vector3 move;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        move = Vector3.zero;
+        if (Input.GetKey(KeyCode.D))
+        {
+            Vector2 move = new Vector2(0.05f, 0f);
+            //Debug.Log(move);
+            //move += transform.right;
+            transform.Translate(move);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            Vector2 move = new Vector2(-0.05f, 0f);
+            transform.Translate(move);
+        }
+    }
+}
