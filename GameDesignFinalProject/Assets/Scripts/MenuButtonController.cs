cb1baad53a4c80feb2347ce03c8328cf2bd86eb6
Samantha Waln <samywaln@gmail.com>
2020-04-07 17:09:22 -0700

    Added main menu

---
+++ GameDesignFinalProject/Assets/Scripts/MenuButtonController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtonController : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Level1");
+        Debug.Log("Loaded scene");
+    }
+}
